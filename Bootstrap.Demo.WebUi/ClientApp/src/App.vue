ed37bc774385e5f40cd2afef971207543945f12d
Søren Lund <slu@commentor.dk>
2021-06-09 09:29:04 +0200

    Create project

---
+++ Bootstrap.Demo.WebUi/ClientApp/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <nav-menu></nav-menu>
+    <router-view />
+</template>
+
+<script>
+    import NavMenu from './components/NavMenu.vue'
+
+    export default {
+        name: 'App',
+        components: {
+            NavMenu
+        }
+    }
+</script>
+
+<style>
+    #app {
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
---
+++ Bootstrap.Demo.WebUi/ClientApp/src/router/index.js
@@ -0,0 +1,29 @@
+﻿import { createWebHistory, createRouter } from "vue-router";
+import Home from "@/pages/Home.vue";
+import Counter from "@/pages/Counter.vue";
+import FetchData from "@/pages/FetchData.vue";
+
+const routes = [
+    {
+        path: "/",
+        name: "Home",
+        component: Home,
+    },
+    {
+        path: "/Counter",
+        name: "Counter",
+        component: Counter,
+    },
+    {
+        path: "/FetchData",
+        name: "FetchData",
+        component: FetchData,
+    }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
